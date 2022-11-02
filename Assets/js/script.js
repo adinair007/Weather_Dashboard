44b5a6d498054eff7c70af8af170c57c6d3d36c8
Aditya Nair <adinair.in@gmail.com>
2022-11-02 14:41:30 -0500

    Started js

---
+++ Assets/js/script.js
@@ -0,0 +1,21 @@
+var locationFormEl = document.getElementById('locationSearch-form');
+var locationInputEl = document.getElementById('location');
+var pastSearchBtnEl = document.getElementById('pastSearches-btn');
+var currentForecastEl = document.getElementById('current-forecast')
+var searchedInputEl = document.getElementById('searched-location');
+var weatherContainerEl = document.getElementById('weather-container');
+var forecastHeaderEl = document.getElementById('forecasr')
+var weekForecastContainerEl = document.getElementById('week-container')
+var locations = [];
+
+
+var weather =
+
+var formSubmission = function(event) {
+    event.preventDefault();
+    var location = locationInputEl.ariaValueMax.trim();
+
+    if(location){
+        location
+    }
+}
