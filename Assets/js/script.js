dc9ded5cadd6de8f4643286ca60a0347eda7bd86
Aditya Nair <adinair.in@gmail.com>
2022-11-02 23:08:06 -0500

    added forecast function to js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -26,9 +26,7 @@ var formSubmission = function(event) {
     pastSearch(city);
 }
 
-var saveSearch = function(){
-    localStorage.setItem("cities", JSON.stringify(cities));
-}
+
 
 //--Fetching OpenWeather Api--
 
@@ -38,7 +36,7 @@ var cityWeather = function(city){
 
     fetch(apiURL).then(function(response){
         response.json().then(function(data){
-            currentForecast(data,city);
+            currentForecast(data, city);
         });
     });
 
@@ -56,5 +54,41 @@ var currentForecast = function(weather, searchCity){
     searchedInputEl.appendChild(currentDay);
 
     //--Displaying current weather icon--
+    var weatherIconEl = document.createElement("span");
+    var weatherIcon = document.createElement("img");
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`);
+    weatherIconEl.appendChild(weatherIcon);
+    searchedInputEl.appendChild(weatherIconEl);
+
+    //--Displaying current temperature--
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    temperatureEl.classList = "list-group-item";
+
+    //--Appending to the main weather-container--
+    weatherContainerEl.appendChild(temperatureEl);
+
+    //--Displaying current humidity--
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+    humidityEl.classList = "list-group-item";
     
+     //--Appending to the main weather-container--
+    weatherContainerEl.appendChild(humidityEl);
+
+    //--Displaying current wind-speed--
+    var windSpeedEl = document.createElement("span");
+    windSpeedEl.textContent = "Wind-Speed: " + weather.wind.speed + " MPH";
+    windSpeedEl.classList = "list-group-item";
+
+    //--Appending to the main weather-container--
+    weatherContainerEl.appendChild(windSpeedEl);
+
 }
+
+
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+cityFormEl.addEventListener("submit", formSubmission);
